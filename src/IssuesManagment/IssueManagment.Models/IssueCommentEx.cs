171b8534bb333978479315209026caee5db432d6
Mordechai Zuber <mez613@gmail.com>
2015-06-08 16:29:16 +0300

    updated models + refactor of request clients
    Request time dropped by approximately 75%

---
+++ src/IssuesManagment/IssueManagment.Models/IssueCommentEx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace IssuesManagment
+{
+    public class IssueCommentEx : IssueComment
+    {
+        public static IssueCommentEx FromIssueComment(IssueComment c)
+        {
+            return new IssueCommentEx
+            {
+                Id = c.Id,
+                Body = c.Body,
+                CreatedAt = c.CreatedAt,
+                HtmlUrl = c.HtmlUrl,
+                UpdatedAt = c.UpdatedAt,
+                Url = c.Url,
+                User = c.User
+            };
+        }
+        public int IssueNumber
+        {
+            get
+            {
+                return int.Parse(HtmlUrl.Segments[4]);
+            }
+        }
+    }
+}
---
+++ src/IssuesManagment/IssueManagment.Models/IssueEx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace IssuesManagment
+{
+    public class IssueEx : Issue
+    {
+        public IReadOnlyList<IssueCommentEx> FullComments { get; set; }
+
+        public IssueEx()
+            : this(new List<IssueCommentEx>())
+        {
+        }
+
+        public IssueEx(IEnumerable<IssueCommentEx> comments)
+        {
+            FullComments = comments.ToList();
+
+        }
+
+        public IssueEx(IEnumerable<IssueComment> comments)
+        {
+            FullComments = comments.Select(c => IssueCommentEx.FromIssueComment(c)).ToList();
+
+        }
+
+        public static IssueEx FromIssue(Issue issue, IEnumerable<IssueCommentEx> comments)
+        {
+            return new IssueEx(comments) 
+            {
+                Assignee = issue.Assignee,
+                Body = issue.Body,
+                ClosedAt = issue.ClosedAt,
+                Comments = issue.Comments,
+                CreatedAt = issue.CreatedAt,
+                HtmlUrl = issue.HtmlUrl,
+                Labels = issue.Labels,
+                Milestone = issue.Milestone,
+                Number = issue.Number,
+                PullRequest = issue.PullRequest,
+                State = issue.State,
+                Title = issue.Title,
+                UpdatedAt = issue.UpdatedAt,
+                Url = issue.Url,
+                User = issue.User
+            };
+        }
+
+        public static IssueEx FromIssue(Issue issue, IEnumerable<IssueComment> comments)
+        {
+            return new IssueEx(comments)
+            {
+                Assignee = issue.Assignee,
+                Body = issue.Body,
+                ClosedAt = issue.ClosedAt,
+                Comments = issue.Comments,
+                CreatedAt = issue.CreatedAt,
+                HtmlUrl = issue.HtmlUrl,
+                Labels = issue.Labels,
+                Milestone = issue.Milestone,
+                Number = issue.Number,
+                PullRequest = issue.PullRequest,
+                State = issue.State,
+                Title = issue.Title,
+                UpdatedAt = issue.UpdatedAt,
+                Url = issue.Url,
+                User = issue.User
+            };
+        }
+    }
+}
